bb3bba159c5b16504f3b42c5d17f6f4a33b42d1f
Ashesh <asheshmandal73@gmail.com>
2024-01-08 19:37:27 +0530

    Connected to postgresql

--- server/server/settings.py
+++ server/server/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': 'pass',
+        'HOST': '172.17.0.2',
+        'PORT': '5432',
     }
 }
 
